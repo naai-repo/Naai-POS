33b1349ed2ea709a1a04891102cdf4184f04a72a
Dtamta007 <dtamta007@gmail.com>
2024-07-10 03:39:57 +0530

    Adds Checkboxed for Exclude GST

--- lib/helper.ts
+++ lib/helper.ts
@@ -1,5 +1,35 @@
 import axios from "axios";
 import { Urls } from "./api";
+import { CustomerInfoInterface } from "./types";
+
+export const sendMessageToUser = async (user: CustomerInfoInterface, message: string) => {
+  try {
+    const body = {
+      Text: `${message} - ${process.env.SENDER_ID}`,
+      Number: user.phoneNumber,
+      SenderId: process.env.SENDER_ID,
+      DRNotifyUrl: "https://www.domainname.com/notifyurl",
+      DRNotifyHttpMethod: "POST",
+      Tool: "API",
+    };
+
+    // Sends SMS OTP to user.
+    const data = await axios.post(
+      `https://restapi.smscountry.com/v0.1/Accounts/${process.env.AUTH_KEY}/SMSes/`,
+      body,
+      {
+        auth: {
+          username: process.env.AUTH_KEY || "",
+          password: process.env.AUTH_TOKEN || "",
+        },
+      }
+    );
+
+    console.log("Message Sent to User!");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const titleArr: string[] = [
   "services",
