85b476d03585396fe2632b5050a6670cc8aab71b
Dtamta007 <dtamta007@gmail.com>
2024-07-12 23:31:14 +0530

    Adds expandable sidebar

--- components/mainWrapper/SideNavigation.tsx
+++ components/mainWrapper/SideNavigation.tsx
@@ -1,20 +1,31 @@
 "use client"
 import React,{ useState } from "react"
 import Link from "next/link"
+import Image from "next/image"
 import {
+  Bolt,
+  ChevronRight,
   Clock,
   Cylinder,
+  FileText,
+  HandCoins,
+  HandPlatter,
   Home,
   LineChart,
+  Menu,
   MessageCircleMore,
   Package,
   Package2,
   Percent,
+  Plus,
   ShoppingCart,
+  Slack,
+  TrendingUp,
   User,
   Users,
   Users2,
   WatchIcon,
+  X,
 } from "lucide-react"
 import {
   Tooltip,
@@ -23,6 +34,8 @@ import {
   TooltipProvider
 } from "@/components/ui/tooltip";
 import AlertDialog from '@/components/alertDialog/AlertDialog';
+import { Button, buttonVariants } from "../ui/button";
+import { cn } from "@/lib/utils";
 
 type MenuType = {
   title : string,
@@ -31,14 +44,70 @@ type MenuType = {
   path? : string
 };
 
-function SideNavigation() {
+const SideNavigation : React.FC<{navigationMenus : MenuType[],isCollapsed : boolean}> = ({navigationMenus,isCollapsed}) => {
   const [open, setOpen] = useState(false);
-   
+
+  return (
+    <>
+        <AlertDialog open={open} setOpen={setOpen} title={"ALERT"} discription={"This Feature Will Be Live in 15 Days"}></AlertDialog>
+        <TooltipProvider>
+            {
+              navigationMenus.map((item,idx) => {
+                let redirectPath = item?.path;
+                if(redirectPath == null) redirectPath = item?.redirectUrl;
+                let isSelected = "POS" == item.title!;
+                return (
+                  <Tooltip key={idx} delayDuration={0}>
+                    <TooltipTrigger asChild>
+                    {/* <div onClick={(e)=>{
+                          if(redirectPath == undefined){
+                              callBackValue.showAlert("ALERT","This Feature Will Be Live In 15 Days");
+                          }
+                      }}> */}
+                  
+                        <Link
+                          onClick={(e) => {
+                            if(redirectPath == undefined){
+                                 setOpen(true);
+                            }
+                          }}
+                          href={redirectPath ?? ""}
+                          className={cn(
+                            buttonVariants({ variant: (isSelected) ? "default" : "outline", size: "icon" }),
+                            "h-8 w-full border-none shadow-none rounded-md",
+                            (isCollapsed) ? "justify-center" : "justify-start p-4",
+                            (isSelected) ? "bg-[#000000] hover:bg-[#000000]/85 text-primary-foreground" : "text-muted-foreground transition-colors hover:text-foreground"
+                            // "default" === "default" &&
+                            //   "dark:bg-muted dark:text-muted-foreground dark:hover:bg-muted dark:hover:text-white"
+                          )}
+                        >
+                          <div className="flex flex-row gap-3">
+                            {item.icon}
+                            {(!isCollapsed) ? item.title : null}
+                          </div>
+                          {/* {(!isCollapsed) ? <span className="sr-only">{item.title}</span> : null} */}
+                        </Link>
+                      {/* </div> */}
+                    </TooltipTrigger>
+                    {/* <TooltipContent side="right">{item.title}</TooltipContent> */}
+                </Tooltip>
+                )
+              })
+            }
+          </TooltipProvider>
+    </>
+
+  )
+}
+
+export const ExpandableNavigation : React.FC = () => {
+  const [data,setData] = useState({isCollapsed : true});
+
   const navigationMenus : MenuType[] = [
     {
       title : "Dashboard",
       icon : <Home className="h-5 w-5" />,
-      redirectUrl : "https://partner.naai.in/dashboard"
+      path : "https://partner.naai.in/dashboard"
     },
     {
       title : "POS",
@@ -48,110 +117,94 @@ function SideNavigation() {
     {
       title : "Analytics",
       icon : <LineChart className="h-5 w-5" />,
-      redirectUrl : "https://partner.naai.in/analytics"
-    },
-    {
-      title : "Profile",
-      icon : <User className="h-5 w-5" />,
-      redirectUrl : "https://partner.naai.in/profile"
+      path : "https://partner.naai.in/analytics"
     },
     {
       title : "Reviews",
       icon : <MessageCircleMore className="h-5 w-5" />,
-      redirectUrl : "https://partner.naai.in/reviews"
+      path : "https://partner.naai.in/profile"
     },
     {
       title : "Add Staff",
-      icon : <Users className="h-5 w-5"/>,
-      redirectUrl : "https://partner.naai.in/staff-management"
+      icon : <Plus className="h-5 w-5"/>,
+      path : "https://partner.naai.in/staff-management"
     },
     {
       title : "Manage Time",
       icon : <Clock className="h-5 w-5"/>,
-      redirectUrl : "https://partner.naai.in/time-management"
+      path : "https://partner.naai.in/time-management"
     },
     {
       title : "Discounts",
       icon : <Percent className="h-5 w-5"/>,
-      redirectUrl : "https://partner.naai.in/discount"
+      path : "https://partner.naai.in/discount"
     },
     {
       title : "Customers",
       icon : <Users2 className="h-5 w-5" />,
-      redirectUrl : "https://partner.naai.in/profile/customer-list"
+      path : "https://partner.naai.in/profile/customer-list"
     },
+    {
+      title : "Settings",
+      icon : <Bolt className="h-5 w-5"/>,
+      path : "https://partner.naai.in/profile"
+    },
+    // {
+    //   title : "Subscriptions",
+    //   icon : <TrendingUp className="h-5 w-5"/>,
+    //   path : router.routes.subscriptionsScreen
+    // },
     {
       title : "Products",
       icon : <Package className="h-5 w-5" />,
     },
     {
-      title : "Orders",
-      icon : <ShoppingCart className="h-5 w-5" />,
+      title : "Services",
+      icon : <HandPlatter className="h-5 w-5" />,
     },
+    {
+      title : "Memberships",
+      icon : <Slack className="h-5 w-5"/>,
+    },
+    {
+      title : "Invoice",
+      icon : <FileText className="h-5 w-5"/>,
+    },
+    {
+      title : "Expenses",
+      icon : <HandCoins className="h-5 w-5"/>,
+    }
   ]
-
 
   return (
     <>
-       <AlertDialog open={open} setOpen={setOpen} title={"ALERT"} discription={"This Feature Will Be Live in 15 Days"}></AlertDialog>
-       <TooltipProvider>
+      <aside onMouseEnter={(e) => {
+        setData({...data, isCollapsed : false })
+      }} onMouseLeave={(e) => {
+        setData({...data, isCollapsed : true })
+      }} className={cn(
+        "fixed inset-y-0 left-0 z-[50] hidden flex-col border-r bg-background sm:flex transition-all",
+         (data.isCollapsed) ? "w-14" : "h-full shadow-lg w-48"
+      )}>
+        <nav className={cn(
+          "flex flex-col h-full w-full transition-all",
+          (data.isCollapsed) ? "items-start gap-4 px-2 sm:py-4" : "items-start gap-4 px-2 sm:py-4 overflow-y-scroll scrollbar-hide",
+        )}>
           {
-            navigationMenus.map((item,idx) => {
-              let redirectPath = item?.path;
-              if(redirectPath == null) redirectPath = item?.redirectUrl;
-
-              return (
-                <Tooltip key={idx}>
-                  <TooltipTrigger asChild >
-                    <div onClick={(e)=>{
-                         if(redirectPath == undefined){
-                            setOpen(true);
-                         }
-                    }}>
-                       <MenuIcon isSelected={item.title === "POS"!} value={item}></MenuIcon>
-                    </div>
-                  </TooltipTrigger>
-                  <TooltipContent side="right">{item.title}</TooltipContent>
-               </Tooltip>
-              )
-            })
+            //items-start gap-4 px-0 sm:py-0
+              // (data.isCollapsed) ? 
+              //   <Button variant="outline" className="py-4 px-2" size="sm" onClick={(e) => {
+              //     setData({...data,isCollapsed : false});
+              //   }}>
+              //     <Menu size={18} ></Menu>
+              //   </Button>
+              // : null
+         }
+          {
+            <SideNavigation navigationMenus={navigationMenus} isCollapsed={data.isCollapsed}></SideNavigation>
           }
-        </TooltipProvider>
+        </nav>
+      </aside>
     </>
   )
 }
-
-const MenuIcon = React.forwardRef<HTMLSpanElement,{isSelected : boolean,value : MenuType}>(({isSelected,value},ref) => {
-  let redirectPath = value?.path;
-  if(redirectPath == null) redirectPath = value?.redirectUrl;
-  if(redirectPath == null) redirectPath = "#";
-
-  return (
-    <>
-    {
-      (isSelected) ? (
-        <Link href="#" className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-[#000000] text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
-          {value.icon}
-          <span ref={ref} className="sr-only">{value.title}</span>
-        </Link>
-      ) : (
-        <Link
-          href={redirectPath}
-          // onClick={(e) => {
-          //   if(redirectPath == null) return;
-          //   router.push(redirectPath);
-          // }}
-          className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
-        >
-          {value.icon}
-          <span ref={ref} className="sr-only">{value.title}</span>
-        </Link>
-      )
-    }
-    </>
-  )
-})
-
-MenuIcon.displayName = 'MenuIcon'; 
-
-export default SideNavigation
--- components/mainWrapper/mainWrapper.tsx
+++ components/mainWrapper/mainWrapper.tsx
@@ -1,6 +1,6 @@
 "use client"
 import React,{ HTMLAttributes } from "react"
-import SideNavigation from "./SideNavigation"
+import { ExpandableNavigation } from "./SideNavigation"
 import MainHeader from "./mainHeader"
 
 interface MainWrapperProps {
@@ -13,9 +13,9 @@ interface MainWrapperProps {
 const MainWrapper :React.FC<MainWrapperProps> = ({children,parentWrapper,mainWrapper,name}) => {
   return (
     <div className="flex h-full w-full flex-col bg-muted/40">
-      <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+      <aside className="fixed inset-y-0 left-0 z-50 hidden w-14 flex-col border-r bg-background sm:flex">
         <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
-          <SideNavigation></SideNavigation>
+          <ExpandableNavigation></ExpandableNavigation>
         </nav>
       </aside>
 
